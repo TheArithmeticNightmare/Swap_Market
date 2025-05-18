cbc652592a8658ba6bb475ac5b5570a62d69dc61
TheArithmeticNightmare <zinyekajoshua427@gmail.com>
2025-05-18 19:21:44 +0200

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+// settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My Application"
+include(":app", ":feature_upload", ":core")
+
